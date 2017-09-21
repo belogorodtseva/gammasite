599333cf32bd463b05241175c59ca995f42b46e8
belogorodtseva <annbelogorodtseva@gmail.com>
2017-09-21 03:16:51 +0300

    models

--- gamma/views.py
+++ gamma/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.shortcuts import get_list_or_404, get_object_or_404
 
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 
 def index(request):
     content = {
@@ -31,8 +32,19 @@ def service(request, pk):
     return render(request, 'service.html', content)
 
 def projects(request):
+    projectlist = Projects.objects.all().order_by("-id")
+    paginator = Paginator(projectlist, 9)
+    page = request.GET.get('page')
+    try:
+        project = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        project = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        project = paginator.page(paginator.num_pages)
     content = {
-        'Projects' : Projects.objects.all(),
+        'Projects' : project,
         'Models' : Models.objects.all(),
     }
     return render(request, 'projects.html', content)
@@ -46,10 +58,20 @@ def project(request, pk):
     return render(request, 'project.html', content)
 
 def gallery(request):
-    content = {
-        'Gallery' : Gallery.objects.all(),
-    }
-    return render(request, 'gallery.html', content)
+    gallerylist = Gallery.objects.all().order_by("-id")
+    paginator = Paginator(gallerylist, 9)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+
+    return render(request, 'gallery.html', {'contacts': contacts})
+
 
 def photogallery(request, pk):
     content = {
@@ -60,10 +82,23 @@ def photogallery(request, pk):
 
 
 def news(request):
+    projectlist = News.objects.all().order_by("-id")
+    paginator = Paginator(projectlist, 9)
+    page = request.GET.get('page')
+    try:
+        project = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        project = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        project = paginator.page(paginator.num_pages)
     content = {
-        'News' : News.objects.all(),
+        'Projects' : project,
+        'Models' : Models.objects.all(),
     }
     return render(request, 'news.html', content)
+
 
 def contact(request):
 
